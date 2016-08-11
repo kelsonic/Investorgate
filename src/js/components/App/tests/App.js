53a3dff8feb89ea00a4ac0928e8dda91747e3f90
kelsonic <kgadams93@gmail.com>
2016-08-11 10:42:29 +0200

    Add app jest test skeleton

---
+++ src/js/components/App/tests/App.js
@@ -0,0 +1,12 @@
+jest.unmock('../App.jsx');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import App from '../App.jsx';
+
+describe('App', () => {
+
+  
+
+});
